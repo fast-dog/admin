cbb5c163c359005231df6fe5899385790769e7d2
Andrey Martynov <d.g.dev482@gmail.com>
2019-05-22 19:44:58 +0500

    Добавлен csrf_token

--- views/admin/dashboard.blade.php
+++ views/admin/dashboard.blade.php
@@ -1,6 +1,7 @@
 <?php
 $html = file_get_contents(public_path('/vendor/fast_dog/frontend/index.html'));
 
-$html = str_replace(['/css/', '/js/'], ['/vendor/fast_dog/frontend/css/', '/vendor/fast_dog/frontend/js/'], $html);
+$html = str_replace(['/css/', '/js/', '{csrf_token}'],
+    ['/vendor/fast_dog/frontend/css/', '/vendor/fast_dog/frontend/js/', csrf_token()], $html);
 
 echo $html;
