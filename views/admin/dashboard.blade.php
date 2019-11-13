6ae8977a71e88d69245b32d49dc75ed73e700c65
dg <d.g.dev482@gmail.com>
2019-11-13 16:44:33 +0500

    add ymap_key

--- views/admin/dashboard.blade.php
+++ views/admin/dashboard.blade.php
@@ -1,7 +1,7 @@
 <?php
 $html = file_get_contents(public_path('/vendor/fast_dog/frontend/index.html'));
 
-$html = str_replace(['/css/', '/js/', '{csrf_token}'],
-    ['/vendor/fast_dog/frontend/css/', '/vendor/fast_dog/frontend/js/', csrf_token()], $html);
+$html = str_replace(['/css/', '/js/', '{csrf_token}', '{YMAP_KEY}'],
+    ['/vendor/fast_dog/frontend/css/', '/vendor/fast_dog/frontend/js/', csrf_token()], $html, env('YMAP_KEY'));
 
 echo $html;
