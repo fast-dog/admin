37be4242a37a4a63bf75bce56e7ddde4607aa52f
dg <d.g.dev482@gmail.com>
2019-11-13 17:00:26 +0500

    fix view

--- views/admin/dashboard.blade.php
+++ views/admin/dashboard.blade.php
@@ -2,6 +2,6 @@
 $html = file_get_contents(public_path('/vendor/fast_dog/frontend/index.html'));
 
 $html = str_replace(['/css/', '/js/', '{csrf_token}', '{YMAP_KEY}'],
-    ['/vendor/fast_dog/frontend/css/', '/vendor/fast_dog/frontend/js/', csrf_token()], $html, env('YMAP_KEY'));
+    ['/vendor/fast_dog/frontend/css/', '/vendor/fast_dog/frontend/js/', csrf_token(), env('YMAP_KEY')], $html);
 
 echo $html;
